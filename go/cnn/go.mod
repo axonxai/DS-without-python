d255df3c0f4232c9a21adfa14d789b5606b35396
Rafael <rafacaropre@gmail.com>
2021-11-16 21:38:51 +0100

    Extra CNN Go

--- go/cnn/go.mod
+++ go/cnn/go.mod
@@ -1,27 +1,27 @@
-module mnist-nn
+module cnn
 
 go 1.17
 
 require (
-	github.com/pkg/errors v0.9.1
-	gonum.org/v1/gonum v0.9.3
-	gorgonia.org/tensor v0.9.21
-	gorgonia.org/vecf64 v0.9.0
-)
-
-require (
-	github.com/apache/arrow/go/arrow v0.0.0-20211006163139-3c5b62c11673 // indirect
+	github.com/apache/arrow/go/arrow v0.0.0-20211111185050-a3746040d8a3 // indirect
+	github.com/awalterschulze/gographviz v2.0.3+incompatible // indirect
 	github.com/chewxy/hm v1.0.0 // indirect
 	github.com/chewxy/math32 v1.0.8 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/flatbuffers v2.0.0+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/leesper/go_rng v0.0.0-20190531154944-a612b043e353 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/xtgo/set v1.0.0 // indirect
-	go4.org/unsafe/assume-no-moving-gc v0.0.0-20201222180813-1025295fd063 // indirect
-	golang.org/x/exp v0.0.0-20191002040644-a1355ae1e2c3 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20211027215541-db492cf91b37 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gonum.org/v1/netlib v0.0.0-20201012070519-2390d26c3658 // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gorgonia.org/cu v0.9.4 // indirect
 	gorgonia.org/dawson v1.2.0 // indirect
+	gorgonia.org/gorgonia v0.9.17 // indirect
+	gorgonia.org/tensor v0.9.21 // indirect
 	gorgonia.org/vecf32 v0.9.0 // indirect
+	gorgonia.org/vecf64 v0.9.0 // indirect
 )
--- go/cnn/mnist.go
+++ go/cnn/mnist.go
@@ -1,15 +1,11 @@
+// package handles the mnist data set
 package main
 
 import (
-	"encoding/binary"
-	"image"
-	"image/color"
-	"image/png"
-	"io"
-	"log"
-	"math"
 	"os"
+	"path/filepath"
 
+	"github.com/pkg/errors"
 	"gorgonia.org/tensor"
 )
 
@@ -20,82 +16,47 @@ type RawImage []byte
 // Label is a digit label in 0 to 9
 type Label uint8
 
-const numLabels = 10
-const pixelRange = 255
+// Load loads the mnist data into two tensors
+//
+// typ can be "train", "test"
+//
+// loc represents where the mnist files are held
+func Load(typ, loc string, as tensor.Dtype) (inputs, targets tensor.Tensor, err error) {
+	const (
+		trainLabel = "train-labels-idx1-ubyte"
+		trainData  = "train-images-idx3-ubyte"
+		testLabel  = "t10k-labels-idx1-ubyte"
+		testData   = "t10k-images-idx3-ubyte"
+	)
 
-const (
-	imageMagic = 0x00000803
-	labelMagic = 0x00000801
-	Width      = 28
-	Height     = 28
-)
-
-var xxx int
-
-func readLabelFile(r io.Reader, e error) (labels []Label, err error) {
-	if e != nil {
-		return nil, e
-	}
-
-	var magic, n int32
-	if err = binary.Read(r, binary.BigEndian, &magic); err != nil {
-		return nil, err
-	}
-	if magic != labelMagic {
-		return nil, os.ErrInvalid
-	}
-	if err = binary.Read(r, binary.BigEndian, &n); err != nil {
-		return nil, err
-	}
-	labels = make([]Label, n)
-	for i := 0; i < int(n); i++ {
-		var l Label
-		if err := binary.Read(r, binary.BigEndian, &l); err != nil {
-			return nil, err
-		}
-		labels[i] = l
-	}
-	return labels, nil
-}
-
-func readImageFile(r io.Reader, e error) (imgs []RawImage, err error) {
-	if e != nil {
-		return nil, e
+	var labelFile, dataFile string
+	switch typ {
+	case "train", "dev":
+		labelFile = filepath.Join(loc, trainLabel)
+		dataFile = filepath.Join(loc, trainData)
+	case "test":
+		labelFile = filepath.Join(loc, testLabel)
+		dataFile = filepath.Join(loc, testData)
 	}
 
-	var magic, n, nrow, ncol int32
-	if err = binary.Read(r, binary.BigEndian, &magic); err != nil {
-		return nil, err
+	var labelData []Label
+	var imageData []RawImage
+
+	if labelData, err = readLabelFile(os.Open(labelFile)); err != nil {
+		return nil, nil, errors.Wrap(err, "Unable to read Labels")
 	}
-	if magic != imageMagic {
-		return nil, err /*os.ErrInvalid*/
+
+	if imageData, err = readImageFile(os.Open(dataFile)); err != nil {
+		return nil, nil, errors.Wrap(err, "Unable to read image data")
 	}
-	if err = binary.Read(r, binary.BigEndian, &n); err != nil {
-		return nil, err
-	}
-	if err = binary.Read(r, binary.BigEndian, &nrow); err != nil {
-		return nil, err
-	}
-	if err = binary.Read(r, binary.BigEndian, &ncol); err != nil {
-		return nil, err
-	}
-	imgs = make([]RawImage, n)
-	m := int(nrow * ncol)
-	for i := 0; i < int(n); i++ {
-		imgs[i] = make(RawImage, m)
-		m_, err := io.ReadFull(r, imgs[i])
-		if err != nil {
-			return nil, err
-		}
-		if m_ != int(m) {
-			return nil, os.ErrInvalid
-		}
-	}
-	return imgs, nil
+
+	inputs = prepareX(imageData, as)
+	targets = prepareY(labelData, as)
+	return
 }
 
 func pixelWeight(px byte) float64 {
-	retVal := (float64(px) / 255 * 0.999) + 0.001
+	retVal := float64(px)/pixelRange*0.9 + 0.1
 	if retVal == 1.0 {
 		return 0.999
 	}
@@ -103,172 +64,72 @@ func pixelWeight(px byte) float64 {
 }
 
 func reversePixelWeight(px float64) byte {
-	return byte(((px - 0.001) / 0.999) * 255)
+	return byte((pixelRange*px - pixelRange) / 0.9)
 }
 
-func prepareX(M []RawImage) (retVal tensor.Tensor) {
+func prepareX(M []RawImage, dt tensor.Dtype) (retVal tensor.Tensor) {
 	rows := len(M)
 	cols := len(M[0])
 
-	b := make([]float64, 0, rows*cols)
-	for i := 0; i < rows; i++ {
-		for j := 0; j < len(M[i]); j++ {
-			b = append(b, pixelWeight(M[i][j]))
+	var backing interface{}
+	switch dt {
+	case tensor.Float64:
+		b := make([]float64, rows*cols, rows*cols)
+		b = b[:0]
+		for i := 0; i < rows; i++ {
+			for j := 0; j < len(M[i]); j++ {
+				b = append(b, pixelWeight(M[i][j]))
+			}
 		}
+		backing = b
+	case tensor.Float32:
+		b := make([]float32, rows*cols, rows*cols)
+		b = b[:0]
+		for i := 0; i < rows; i++ {
+			for j := 0; j < len(M[i]); j++ {
+				b = append(b, float32(pixelWeight(M[i][j])))
+			}
+		}
+		backing = b
 	}
-	return tensor.New(tensor.WithShape(rows, cols), tensor.WithBacking(b))
+	retVal = tensor.New(tensor.WithShape(rows, cols), tensor.WithBacking(backing))
+	return
 }
 
-func prepareY(N []Label) (retVal tensor.Tensor) {
+func prepareY(N []Label, dt tensor.Dtype) (retVal tensor.Tensor) {
 	rows := len(N)
 	cols := 10
 
-	b := make([]float64, 0, rows*cols)
-	for i := 0; i < rows; i++ {
-		for j := 0; j < 10; j++ {
-			if j == int(N[i]) {
-				b = append(b, 0.999)
-			} else {
-				b = append(b, 0.001)
+	var backing interface{}
+	switch dt {
+	case tensor.Float64:
+		b := make([]float64, rows*cols, rows*cols)
+		b = b[:0]
+		for i := 0; i < rows; i++ {
+			for j := 0; j < 10; j++ {
+				if j == int(N[i]) {
+					b = append(b, 0.9)
+				} else {
+					b = append(b, 0.1)
+				}
 			}
 		}
-	}
-	return tensor.New(tensor.WithShape(rows, cols), tensor.WithBacking(b))
-}
-
-// visualize visualizes the first N images given a data tensor that is made up of float64s.
-// It's arranged into (rows, 10) image.
-// Row counts are calculated by dividing N by 10 - we only ever want 10 columns.
-// For simplicity's sake, we will truncate any remainders.
-func visualize(data tensor.Tensor, rows, cols int, filename string) (err error) {
-	N := rows * cols
-
-	sliced := data
-	if N > 1 {
-		sliced, err = data.Slice(makeRS(0, N), nil) // data[0:N, :] in python
-		if err != nil {
-			return err
-		}
-	}
-
-	if err = sliced.Reshape(rows, cols, 28, 28); err != nil {
-		return err
-	}
-
-	imCols := 28 * cols
-	imRows := 28 * rows
-	rect := image.Rect(0, 0, imCols, imRows)
-	canvas := image.NewGray(rect)
-
-	for i := 0; i < cols; i++ {
-		for j := 0; j < rows; j++ {
-			var patch tensor.Tensor
-			if patch, err = sliced.Slice(makeRS(i, i+1), makeRS(j, j+1)); err != nil {
-				return err
-			}
-
-			patchData := patch.Data().([]float64)
-			for k, px := range patchData {
-				x := j*28 + k%28
-				y := i*28 + k/28
-				c := color.Gray{reversePixelWeight(px)}
-				canvas.Set(x, y, c)
+		backing = b
+	case tensor.Float32:
+		b := make([]float32, rows*cols, rows*cols)
+		b = b[:0]
+		for i := 0; i < rows; i++ {
+			for j := 0; j < 10; j++ {
+				if j == int(N[i]) {
+					b = append(b, 0.9)
+				} else {
+					b = append(b, 0.1)
+				}
 			}
 		}
+		backing = b
+
 	}
-
-	var f io.WriteCloser
-	if f, err = os.Create(filename); err != nil {
-		return err
-	}
-
-	if err = png.Encode(f, canvas); err != nil {
-		f.Close()
-		return err
-	}
-
-	if err = f.Close(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func normalize(data tensor.Tensor) {
-	raw := data.Data().([]float64)
-
-	min, max := math.Inf(1), math.Inf(-1)
-	for _, v := range raw {
-		if v > max {
-			max = v
-		}
-		if v < min {
-			min = v
-		}
-	}
-	for i, v := range raw {
-		raw[i] = v - min/(max-min)
-	}
-}
-
-func visualizeWeights(w tensor.Tensor, rows, cols int, filename string) (err error) {
-
-	s := w.Shape()
-	log.Printf("s %v", s)
-
-	var rehsapedTo []int
-	var patchSide int
-	switch s[1] {
-	case 784:
-		rehsapedTo = []int{rows, cols, 28, 28}
-		patchSide = 28
-	case 100:
-		rehsapedTo = []int{rows, cols, 10, 10}
-		patchSide = 10
-	}
-
-	sliced := w
-	if err = sliced.Reshape(rehsapedTo...); err != nil {
-		log.Printf("Err %v", err)
-		return err
-	}
-
-	imCols := patchSide * cols
-	imRows := patchSide * rows
-	rect := image.Rect(0, 0, imCols, imRows)
-	canvas := image.NewGray(rect)
-
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			var patch tensor.Tensor
-			if patch, err = sliced.Slice(makeRS(i, i+1), makeRS(j, j+1)); err != nil {
-				log.Printf("FAILED TO SLICE i %d j %d ERR : %v | %v", i, j, err, sliced.Shape())
-				return err
-			}
-
-			patchData := patch.Data().([]float64)
-			for k, px := range patchData {
-				x := j*patchSide + k%patchSide
-				y := i*patchSide + k/patchSide
-				c := color.Gray{reversePixelWeight(px)}
-				canvas.Set(x, y, c)
-			}
-		}
-	}
-
-	var f io.WriteCloser
-	if f, err = os.Create(filename); err != nil {
-		log.Printf("FAILED TO CREATE FILE %v", filename)
-		return err
-	}
-
-	if err = png.Encode(f, canvas); err != nil {
-		log.Printf("FAILED TO ENCODE")
-		f.Close()
-		return err
-	}
-
-	if err = f.Close(); err != nil {
-		return err
-	}
-	return nil
+	retVal = tensor.New(tensor.WithShape(rows, cols), tensor.WithBacking(backing))
+	return
 }
